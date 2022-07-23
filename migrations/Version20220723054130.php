0a49685dc7c55dc02c74f98a97e902783f264ed6
Schenkel <thi.schenkel@gmail.com>
2022-07-23 07:49:17 +0200

    try1

--- migrations/Version20220723054130.php
+++ migrations/Version20220723054130.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220722174824 extends AbstractMigration
+final class Version20220723054130 extends AbstractMigration
 {
     public function getDescription(): string
     {
